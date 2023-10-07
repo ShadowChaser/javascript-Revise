2d6e4dcbf13bb519a2ca5f30a506ba2cd443bc55
Abhishek Kundu <30345922+ShadowChaser@users.noreply.github.com>
2023-10-07 16:35:03 +0000

    let,var,const

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId=1231
+let accountEmail="abhi"
+var accountPassword=234242
+accountCity="baalsore"
+// accountId=2  //Not allowed to change const data
+accountEmail="abhishek"
+accountPassword=12355
+accountCity="Bengaluru"
+console.log(accountId);
+console.table([accountId,accountPassword,accountId,accountCity])
+/*
+prefer not to use var 
+because of issue in block score and functional scope 
+*/ 
