340b723726be30192b5a9a16e776cf7b088bdf8f
Abhishek Kundu <30345922+ShadowChaser@users.noreply.github.com>
2023-11-05 03:09:36 +0000

    array

---
+++ Arrays/array01.js
@@ -0,0 +1,8 @@
+let arr=[0,1,2,3,4,5];
+console.log('A',arr);
+let arr1=arr.slice(1,3);
+console.log(arr1);
+console.log('B',arr);
+let arr2=arr.splice(1,3);
+console.log(arr2);
+console.log('C',arr);
